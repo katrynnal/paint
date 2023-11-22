efab87d252330687a41b9cacdb6cd23f6ee8ab2a
katrynnal <151770141+katrynnal@users.noreply.github.com>
2023-11-22 13:38:35 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600, 600);
+ background("#9E9E9EC1");}
+
+function draw() {
+stroke("pink")
+strokeWeight(1); 
+ fill("##03A9F4F3")  
+ if(mouseIsPressed){
+  rect(mouseX,mouseY,20,20);}
+}
